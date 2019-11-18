ae24ae635bd5bbd66e96a114b596502652da9a62
Nicolas6272 <nicolas.jacqusi@gmail.com>
2019-11-18 17:23:36 +0100

    Supression traitement

--- Module_Connexion/Vue_Connexion.php
+++ Module_Connexion/Vue_Connexion.php
@@ -10,20 +10,31 @@
 		function afficheFormulaireCreationCompte() {
 
 		?>
-		<form method="post" action="./index.php?action=ajoutMotardBD">
-		
-		<label>Nom</label>
-		<input type="text" name="Nom"required><br>
-		<label>Prenom</label>
-		<input type="text" name="Prenom"required><br>
-		<label>Mail</label>
-		<input type="email" name="Mail"required><br>
-		<label>Mot de passe</label>
-		<input type="password" name="MotDePasse"required><br>
-		<input type="submit" value="Crée mon compte">
- 	
 
-		</form>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Creation de compte</title>
+			<link rel="stylesheet" type="text/css" href="FormulaireCreationCompteMotard.css">
+		</head>
+		<body>
+		
+			<form method="post" action="./index.php?action=ajoutMotardBD">
+			
+				<label>Nom</label>
+				<input type="text" name="Nom"required><br>
+				<label>Prenom</label>
+				<input type="text" name="Prenom"required><br>
+				<label>Mail</label>
+				<input type="email" name="Mail"required><br>
+				<label>Mot de passe</label>
+				<input type="password" name="MotDePasse"required><br>
+				<input type="submit" value="Crée mon compte">
+
+			</form>
+
+		</body>
+		</html>
 		<?php
 
 		
