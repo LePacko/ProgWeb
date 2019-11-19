8f7b6d3de8fc72e65e1d1f3ebbb7aafa84bcacfb
LePacko <canovas.romain@gmail.com>
2019-11-19 11:32:58 +0100

    le script lamolle

--- scriptBase.sql
+++ scriptBase.sql
@@ -10,13 +10,13 @@
 CREATE TABLE motard(
         id_motard     Int  Auto_increment  NOT NULL ,
         nom           Varchar (64) NOT NULL ,
-        Prenom        Varchar (64) NOT NULL ,
+        prenom        Varchar (64) NOT NULL ,
         adresse       Varchar (128) NOT NULL ,
         code_postal   Int NOT NULL ,
         mail          Varchar (128) NOT NULL ,
         numero_de_tel Int NOT NULL ,
         permis        Varchar (3) NOT NULL ,
-        mdpMotard     Varchar (128) NOT NULL
+        mdp           Varchar (268) NOT NULL
 	,CONSTRAINT motard_PK PRIMARY KEY (id_motard)
 )ENGINE=InnoDB;
 
@@ -32,7 +32,7 @@ CREATE TABLE entreprise(
         code_postale       Int NOT NULL ,
         numero_tel         Int NOT NULL ,
         date_d_affiliation Date NOT NULL ,
-        mdpEntreprise      Varchar (128) NOT NULL
+        mdp                Varchar (268) NOT NULL
 	,CONSTRAINT entreprise_PK PRIMARY KEY (SIRET)
 )ENGINE=InnoDB;
 
@@ -61,36 +61,16 @@ CREATE TABLE circuit(
 CREATE TABLE session(
         id_session     Int  Auto_increment  NOT NULL ,
         date           Date NOT NULL ,
-        duree          Int NOT NULL ,
         nb_place       Int NOT NULL ,
         tarif          Int NOT NULL ,
         status         Varchar (50) NOT NULL ,
         nb_participant Int NOT NULL ,
-        SIRET          Int NOT NULL ,
+        heure_debut    Time NOT NULL ,
+        heure_fin      Time NOT NULL ,
         id_circuit     Int NOT NULL
 	,CONSTRAINT session_PK PRIMARY KEY (id_session)
 
-	,CONSTRAINT session_entreprise_FK FOREIGN KEY (SIRET) REFERENCES entreprise(SIRET)
-	,CONSTRAINT session_circuit0_FK FOREIGN KEY (id_circuit) REFERENCES circuit(id_circuit)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: moto
-#------------------------------------------------------------
-
-CREATE TABLE moto(
-        immatriculation Varchar (10) NOT NULL ,
-        marque          Varchar (50) NOT NULL ,
-        modele          Varchar (50) NOT NULL ,
-        cylindree       Int NOT NULL ,
-        annee           Date NOT NULL ,
-        type            Varchar (50) NOT NULL ,
-        puissance       Int NOT NULL ,
-        id_motard       Int NOT NULL
-	,CONSTRAINT moto_PK PRIMARY KEY (immatriculation)
-
-	,CONSTRAINT moto_motard_FK FOREIGN KEY (id_motard) REFERENCES motard(id_motard)
+	,CONSTRAINT session_circuit_FK FOREIGN KEY (id_circuit) REFERENCES circuit(id_circuit)
 )ENGINE=InnoDB;
 
 
@@ -112,15 +92,46 @@ CREATE TABLE avis(
 
 
 #------------------------------------------------------------
+# Table: modele moto
+#------------------------------------------------------------
+
+CREATE TABLE modele_moto(
+        marque    Varchar (64) NOT NULL ,
+        modele    Varchar (64) NOT NULL ,
+        cylindree Int NOT NULL ,
+        type      Int  Auto_increment  NOT NULL ,
+        puissance Int NOT NULL
+	,CONSTRAINT modele_moto_PK PRIMARY KEY (marque,modele)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: moto
+#------------------------------------------------------------
+
+CREATE TABLE moto(
+        immatriculation Varchar (10) NOT NULL ,
+        annee           Date NOT NULL ,
+        id_motard       Int NOT NULL ,
+        marque          Varchar (64) ,
+        modele          Varchar (64)
+	,CONSTRAINT moto_PK PRIMARY KEY (immatriculation)
+
+	,CONSTRAINT moto_motard_FK FOREIGN KEY (id_motard) REFERENCES motard(id_motard)
+	,CONSTRAINT moto_modele_moto0_FK FOREIGN KEY (marque,modele) REFERENCES modele_moto(marque,modele)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
 # Table: Effectuer
 #------------------------------------------------------------
 
 CREATE TABLE Effectuer(
         id_session Int NOT NULL ,
-        id_motard  Int NOT NULL
+        id_motard  Int NOT NULL ,
+        temps_tour Int NOT NULL
 	,CONSTRAINT Effectuer_PK PRIMARY KEY (id_session,id_motard)
 
 	,CONSTRAINT Effectuer_session_FK FOREIGN KEY (id_session) REFERENCES session(id_session)
 	,CONSTRAINT Effectuer_motard0_FK FOREIGN KEY (id_motard) REFERENCES motard(id_motard)
 )ENGINE=InnoDB;
-
