326866c4a223dd8fdc4f909a40c429c640bc528c
Nicolas Jacquis <nicolas.jacquis@gmail.com>
2019-11-19 09:44:14 +0100

    Inscription+Ajout nouveau inscrit bd

---
+++ Module_Connexion/AfficheFormulaireMotard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Creation de compte</title>
+			<link rel="stylesheet" type="text/css" href="FormulaireCreationCompteMotard.css">
+		</head>
+		<body>
+		
+			<form method="post" action="./index.php?action=ajoutMotardBD">
+			
+				<label>Nom</label>
+				<input type="text" name="Nom"required><br>
+				<label>Prenom</label>
+				<input type="text" name="Prenom"required><br>
+				<label>Mail</label>
+				<input type="email" name="Mail"required><br>
+				<label>Mot de passe</label>
+				<input type="password" name="MotDePasse"required><br>
+				<input type="submit" value="Crée mon compte">
+
+			</form>
+
+		</body>
+		</html>
