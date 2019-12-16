b925f610238dcf43754a5eca75eeb06e30f50a74
NAEL TALBI <ntalbi@a101-19.iut.univ-paris8.fr>
2019-12-16 10:24:07 +0100

    version finale

--- Module/Module_Connexion/Modele_Connexion.php
+++ Module/Module_Connexion/Modele_Connexion.php
@@ -1,7 +1,7 @@
 <?php 
 	
 include_once("./ConnexionBD_iut.php");
-	class Modele_Connexion extends ConnexionBD_iut {
+	class Modele_Connexion extends Connexion {
 
 		function __construct() {
 			parent::init();
