97671a9fc964e58094f7c14eb35b3402db16bb18
Nicolas Jacquis <nicolas.jacquis@gmail.com>
2019-12-17 10:39:20 +0100

    Add Liste Circuit

--- Module/Module_Gerant/Modele_Gerant.php
+++ Module/Module_Gerant/Modele_Gerant.php
@@ -8,6 +8,8 @@ include_once("./Connexion.php");
 			
 		}
 
+		
+
 	}
 
 
--- Module/Module_Motard/Vue_Motard.php
+++ Module/Module_Motard/Vue_Motard.php
@@ -9,7 +9,24 @@
 
 		function formulaireMotard () {
 			echo '';
-		}		
+		}	
+		
+		function Circuit($tableauCircuit) {
+			
+			$nbcircuit = count($tableauCircuit) - count($tableauCircuit[0]) . '<br>';
+			// for($i = 0; $i<	count($tableauCircuit); $i++) {
+			// 	echo $tableauCircuit[0][0];
+			// }
+			
+			for ($j = 0; $j<$nbcircuit; $j++) {
+				$tab = $tableauCircuit[$j];
+				foreach($tab as $i => $value) {
+					echo $value . ' ';
+					
+				}
+				echo '<br>';
+			}
+		}
 	}
 		
 ?>
