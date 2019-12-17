4f2f58430ffb77cee7f18444b970f13d669ab9ed
LePacko <canovas.romain@gmail.com>
2019-12-17 12:46:32 +0100

    ameliore ajout de circuit et vu pour mes circuits

--- Module/Module_Gerant/Vue_Gerant.php
+++ Module/Module_Gerant/Vue_Gerant.php
@@ -16,19 +16,26 @@
 				echo'Vous n avez pas de circuit <br>';
 			}
 			else{
-			$nbcircuit = count($tableauCircuit) - count($tableauCircuit[0]) . '<br>';
-			// for($i = 0; $i<	count($tableauCircuit); $i++) {
-			// 	echo $tableauCircuit[0][0];
-			// }
-
-			for ($j = 0; $j<$nbcircuit; $j++) {
-				$tab = $tableauCircuit[$j];
-				foreach($tab as $i => $value) {
-					echo $value . ' ';
-					
+				$valeurValide=true;
+				$nbcircuit = count($tableauCircuit) - count($tableauCircuit[0]) . '<br>';
+				// for($i = 0; $i<	count($tableauCircuit); $i++) {
+				// 	echo $tableauCircuit[0][0];
+				// }
+				for ($j = 0; $j<$nbcircuit; $j++) {
+					$tab = $tableauCircuit[$j];
+					if($tab[5]==$_SESSION['siret']){
+						$valeurValide=false;
+						foreach($tab as $i => $value) {
+								if($i!=5)
+									echo $value . ' ';
+							
+						}
+					echo '<br>';
+					}
 				}
-				echo '<br>';
-			}
+				if($valeurValide){
+					echo'Vous n avez pas de circuit <br>';
+				}
 		}
 			echo'<a href="./index.php?module=Gerant&action=formajout"> ajout un circuit</a>';
 		}	
